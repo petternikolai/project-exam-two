4ab11ed1f2379e4c40080a418624abadbb7067ae
petternikolai <petkri14832@stud.noroff.no>
2024-11-07 11:30:08 +0100

    Add date-fns dependency and implement useFetch hook for API data retrieval

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [venues, setVenues] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setVenues(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { venues, loading, error };
+};
+
+export default useFetch;
