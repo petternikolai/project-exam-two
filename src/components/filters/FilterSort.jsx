4e99152edcd999942311ae9364e00f9278952742
petternikolai <petkri14832@stud.noroff.no>
2024-11-08 10:20:07 +0100

    implement venue filtering in Venues component

--- src/components/filters/FilterSort.jsx
+++ src/components/filters/FilterSort.jsx
@@ -9,37 +9,41 @@ import {
 } from "@headlessui/react";
 import { ChevronDownIcon, FunnelIcon } from "@heroicons/react/20/solid";
 
-const filters = {
+const filtersData = {
   price: [
-    { value: "0", label: "$0 - $25", checked: false },
-    { value: "25", label: "$25 - $50", checked: false },
-    { value: "50", label: "$50 - $75", checked: false },
-    { value: "75", label: "$75+", checked: false },
+    { value: "0-500", label: "$0 - $500" },
+    { value: "500-1000", label: "$500 - $1000" },
+    { value: "1000-2000", label: "$1000 - $2000" },
+    { value: "2000+", label: "$2000+" },
   ],
-  color: [
-    { value: "white", label: "White", checked: false },
-    { value: "beige", label: "Beige", checked: false },
-    { value: "blue", label: "Blue", checked: true },
-    { value: "brown", label: "Brown", checked: false },
-    { value: "green", label: "Green", checked: false },
-    { value: "purple", label: "Purple", checked: false },
+  maxGuests: [
+    { value: "1-2", label: "1-2 Guests" },
+    { value: "3-4", label: "2-4 Guests" },
+    { value: "5-6", label: "5-6 Guests" },
+    { value: "7+", label: "7+ Guests" },
   ],
-  size: [
-    { value: "xs", label: "XS", checked: false },
-    { value: "s", label: "S", checked: true },
-    { value: "m", label: "M", checked: false },
-    { value: "l", label: "L", checked: false },
-    { value: "xl", label: "XL", checked: false },
-    { value: "2xl", label: "2XL", checked: false },
+  rating: [
+    { value: "1", label: "1 star" },
+    { value: "2", label: "2 stars" },
+    { value: "3", label: "3 stars" },
+    { value: "4", label: "4 stars" },
+    { value: "5", label: "5 stars" },
   ],
-  category: [
-    { value: "all-new-arrivals", label: "All New Arrivals", checked: false },
-    { value: "tees", label: "Tees", checked: false },
-    { value: "objects", label: "Objects", checked: false },
-    { value: "sweatshirts", label: "Sweatshirts", checked: false },
-    { value: "pants-and-shorts", label: "Pants & Shorts", checked: false },
+  amenities: [
+    { value: "wifi", label: "WiFi" },
+    { value: "parking", label: "Parking" },
+    { value: "breakfast", label: "Breakfast" },
+    { value: "pets", label: "Pets" },
   ],
 };
+
+const filterLabels = {
+  price: "Price",
+  maxGuests: "Capacity", // Change the label for maxGuests
+  rating: "Rating",
+  amenities: "Amenities",
+};
+
 const sortOptions = [
   { name: "Most Popular", href: "#", current: true },
   { name: "Best Rating", href: "#", current: false },
@@ -50,19 +54,38 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function FilterSort() {
+export default function FilterSort({ filters, setFilters, clearFilters }) {
+  const handleFilterChange = (category, value) => {
+    setFilters((prevFilters) => {
+      const newFilters = { ...prevFilters };
+      if (newFilters[category].includes(value)) {
+        newFilters[category] = newFilters[category].filter((v) => v !== value);
+      } else {
+        newFilters[category].push(value);
+      }
+      console.log("Updated filters:", newFilters);
+      return newFilters;
+    });
+  };
+
+  const countSelectedFilters = () => {
+    if (!filters) return 0;
+    return Object.values(filters).reduce(
+      (acc, filter) => acc + filter.length,
+      0
+    );
+  };
+
   return (
     <div className="bg-white">
       <div className="px-4 py-16 text-center sm:px-6 lg:px-8">
-        <h1 className="text-5xl font-bold tracking-tight text-gray-900">
-          Stays
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          It's time to find your perfect stay!
         </h1>
         <p className="mx-auto mt-4 max-w-xl text-base text-gray-500">
-          Your dream stay is just a few clicks away.
+          Use the filters below to narrow down your search.
         </p>
       </div>
-
-      {/* Filters */}
       <Disclosure
         as="section"
         aria-labelledby="filter-heading"
@@ -79,125 +102,56 @@ export default function FilterSort() {
                   aria-hidden="true"
                   className="mr-2 h-5 w-5 flex-none text-gray-400 group-hover:text-gray-500"
                 />
-                2 Filters
+                Filters ({countSelectedFilters()})
               </DisclosureButton>
             </div>
             <div className="pl-6">
-              <button type="button" className="text-gray-500">
+              <button
+                type="button"
+                className="text-gray-500"
+                onClick={clearFilters}
+              >
                 Clear all
               </button>
             </div>
           </div>
         </div>
         <DisclosurePanel className="border-t border-gray-200 py-10">
-          <div className="mx-auto grid max-w-7xl grid-cols-2 gap-x-4 px-4 text-sm sm:px-6 md:gap-x-6 lg:px-8">
-            <div className="grid auto-rows-min grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-6">
-              <fieldset>
-                <legend className="block font-medium">Price</legend>
-                <div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
-                  {filters.price.map((option, optionIdx) => (
-                    <div
-                      key={option.value}
-                      className="flex items-center text-base sm:text-sm"
-                    >
-                      <input
-                        defaultValue={option.value}
-                        defaultChecked={option.checked}
-                        id={`price-${optionIdx}`}
-                        name="price[]"
-                        type="checkbox"
-                        className="h-4 w-4 shrink-0 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                      <label
-                        htmlFor={`price-${optionIdx}`}
-                        className="ml-3 min-w-0 flex-1 text-gray-600"
+          <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-2 px-4 text-sm sm:px-6 md:gap-x-3 lg:px-8">
+            <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-4">
+              {Object.keys(filtersData).map((category) => (
+                <fieldset key={category} className="flex flex-col mb-4">
+                  <legend className="block font-medium">
+                    {filterLabels[category]}
+                  </legend>
+                  <div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
+                    {filtersData[category].map((option) => (
+                      <div
+                        key={option.value}
+                        className="flex items-center text-base sm:text-sm"
                       >
-                        {option.label}
-                      </label>
-                    </div>
-                  ))}
-                </div>
-              </fieldset>
-              <fieldset>
-                <legend className="block font-medium">Color</legend>
-                <div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
-                  {filters.color.map((option, optionIdx) => (
-                    <div
-                      key={option.value}
-                      className="flex items-center text-base sm:text-sm"
-                    >
-                      <input
-                        defaultValue={option.value}
-                        defaultChecked={option.checked}
-                        id={`color-${optionIdx}`}
-                        name="color[]"
-                        type="checkbox"
-                        className="h-4 w-4 shrink-0 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                      <label
-                        htmlFor={`color-${optionIdx}`}
-                        className="ml-3 min-w-0 flex-1 text-gray-600"
-                      >
-                        {option.label}
-                      </label>
-                    </div>
-                  ))}
-                </div>
-              </fieldset>
-            </div>
-            <div className="grid auto-rows-min grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-6">
-              <fieldset>
-                <legend className="block font-medium">Size</legend>
-                <div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
-                  {filters.size.map((option, optionIdx) => (
-                    <div
-                      key={option.value}
-                      className="flex items-center text-base sm:text-sm"
-                    >
-                      <input
-                        defaultValue={option.value}
-                        defaultChecked={option.checked}
-                        id={`size-${optionIdx}`}
-                        name="size[]"
-                        type="checkbox"
-                        className="h-4 w-4 shrink-0 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                      <label
-                        htmlFor={`size-${optionIdx}`}
-                        className="ml-3 min-w-0 flex-1 text-gray-600"
-                      >
-                        {option.label}
-                      </label>
-                    </div>
-                  ))}
-                </div>
-              </fieldset>
-              <fieldset>
-                <legend className="block font-medium">Category</legend>
-                <div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
-                  {filters.category.map((option, optionIdx) => (
-                    <div
-                      key={option.value}
-                      className="flex items-center text-base sm:text-sm"
-                    >
-                      <input
-                        defaultValue={option.value}
-                        defaultChecked={option.checked}
-                        id={`category-${optionIdx}`}
-                        name="category[]"
-                        type="checkbox"
-                        className="h-4 w-4 shrink-0 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                      <label
-                        htmlFor={`category-${optionIdx}`}
-                        className="ml-3 min-w-0 flex-1 text-gray-600"
-                      >
-                        {option.label}
-                      </label>
-                    </div>
-                  ))}
-                </div>
-              </fieldset>
+                        <input
+                          value={option.value}
+                          checked={filters?.[category]?.includes(option.value)}
+                          onChange={() =>
+                            handleFilterChange(category, option.value)
+                          }
+                          id={`${category}-${option.value}`}
+                          name={`${category}[]`}
+                          type="checkbox"
+                          className="h-4 w-4 shrink-0 rounded border-gray-300 text-accent focus:ring-accent"
+                        />
+                        <label
+                          htmlFor={`${category}-${option.value}`}
+                          className="ml-3 min-w-0 flex-1 text-gray-600"
+                        >
+                          {option.label}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                </fieldset>
+              ))}
             </div>
           </div>
         </DisclosurePanel>
--- src/pages/Venues.jsx
+++ src/pages/Venues.jsx
@@ -1,13 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { API_BASE_URL, API_VENUES_URL } from "../constants/apiUrls";
 import FilterSort from "../components/filters/FilterSort";
 import VenueSkeleton from "../components/loaders/VenueSkeleton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function Venues() {
-  const { venues, loading, error } = useFetch(
-    `${API_BASE_URL}${API_VENUES_URL}`
-  );
+  const {
+    data: venues,
+    loading,
+    error,
+  } = useFetch(`${API_BASE_URL}${API_VENUES_URL}`);
+  const [filters, setFilters] = useState({
+    price: [],
+    maxGuests: [],
+    rating: [],
+    amenities: [],
+  });
+
+  const clearFilters = () => {
+    setFilters({
+      price: [],
+      maxGuests: [],
+      rating: [],
+      amenities: [],
+    });
+  };
+
+  const applyFilters = (venues) => {
+    if (!venues) return [];
+    console.log("Applying filters:", filters);
+    return venues.data.filter((venue) => {
+      // Apply price filter
+      if (filters.price.length > 0) {
+        const price = parseFloat(venue.price);
+        if (
+          !filters.price.some((range) => {
+            if (range === "2000+") {
+              return price >= 2000;
+            }
+            const [min, max] = range.split("-").map(Number);
+            return price >= min && (max ? price <= max : true);
+          })
+        ) {
+          return false;
+        }
+      }
+      // Apply maxGuests filter
+      if (filters.maxGuests.length > 0) {
+        const guests = parseInt(venue.maxGuests);
+        if (
+          !filters.maxGuests.some((range) => {
+            if (range === "7+") {
+              return guests >= 7;
+            }
+            const [min, max] = range.split("-").map(Number);
+            return guests >= min && (max ? guests <= max : true);
+          })
+        ) {
+          return false;
+        }
+      }
+      // Apply rating filter
+      if (
+        filters.rating.length > 0 &&
+        !filters.rating.includes(venue.rating.toString())
+      ) {
+        return false;
+      }
+      // Apply amenities filter
+      if (
+        filters.amenities.length > 0 &&
+        !filters.amenities.every((amenity) => venue.meta[amenity])
+      ) {
+        return false;
+      }
+      return true;
+    });
+  };
 
   if (loading) {
     return <VenueSkeleton />;
@@ -17,13 +88,20 @@ export default function Venues() {
     return <p>{error.message}</p>;
   }
 
+  console.log("Venues data:", venues);
+  const filteredVenues = applyFilters(venues);
+
   return (
     <>
-      <FilterSort />
+      <FilterSort
+        filters={filters}
+        setFilters={setFilters}
+        clearFilters={clearFilters}
+      />
       <div className="bg-white">
         <div className="mx-auto max-w-7xl overflow-hidden px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 mt-8 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
-            {venues.data.map((venue) => (
+            {filteredVenues.map((venue) => (
               <a
                 key={venue.id}
                 href={venue.href}
@@ -44,15 +122,28 @@ export default function Venues() {
                     />
                   )}
                 </div>
-                <h3 className="mt-4 font-medium text-gray-900">{venue.name}</h3>
-                {venue.location.city ? (
+                <div className="flex justify-between items-center">
+                  <h3 className="mt-4 font-medium text-gray-900">
+                    {venue.name}
+                  </h3>
+                  <div className="flex items-center">
+                    <FontAwesomeIcon
+                      icon={faStar}
+                      className="text-accent mt-3 text-lg mr-2"
+                    />
+                    <p className="mt-4 text-gray-900">
+                      {venue.rating.toFixed(1)}
+                    </p>
+                  </div>
+                </div>
+                {venue.location.country && venue.location.city ? (
                   <p className="mt-2 text-gray-600">
-                    Location: {venue.location.city}
+                    {venue.location.city}, {venue.location.country}
                   </p>
                 ) : (
-                  <p className="mt-2 text-gray-600">Location: Not specified</p>
+                  ""
                 )}
-                <p className="mt-2 text-gray-600">Price: ${venue.price}</p>
+                <p className="mt-2 text-gray-600">${venue.price} per night</p>
               </a>
             ))}
           </div>
